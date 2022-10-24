75c8de27260c0f1ebbf5787dc0f94ebb9a655cca
ingyiramarcela <ingyiramarcela@gmail.com>
2022-10-24 14:22:10 -0500

    (create branch fix/eslint-windows-end-line)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,41 @@
 module.exports = {
-     env: {
-          browser: true,
-          es2021: true,
-     },
-     extends: [
-          'airbnb',
-          'airbnb/hooks',
-          'airbnb-typescript',
-          'plugin:react/recommended',
-          'plugin:@typescript-eslint/recommended',
-          'plugin:react/jsx-runtime',
-          'plugin:prettier/recommended',
-     ],
-     overrides: [],
-     parser: '@typescript-eslint/parser',
-     parserOptions: {
-          ecmaVersion: 'latest',
-          sourceType: 'module',
-          project: ['tsconfig.json'],
-     },
-     plugins: ['react', '@typescript-eslint', 'prettier'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json'],
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'react/prop-types': 0,
+    'react/function-component-definition': [
+      0,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+  },
 }
